2e9e707d377d3aa4f7bf59335e834ca1a6e2ea98
Catherine Hao <31458785+tigerisbigcat@users.noreply.github.com>
2021-02-12 17:44:57 -0800

    Update EmptyQueueException.java

--- Exercise3-LinkedDeque/EmptyQueueException.java
+++ Exercise3-LinkedDeque/EmptyQueueException.java
@@ -1,1 +1,21 @@
-//package Deque.DoublyLinked;package com.company;/**   A class of runtime exceptions thrown by methods to   indicate that a queue is empty.   @author Frank M. Carrano   @author Timothy M. Henry*/public class EmptyQueueException extends RuntimeException{   public EmptyQueueException()   {      this(null);   } // end default constructor      public EmptyQueueException(String message)   {      super(message);   } // end constructor} // end EmptyQueueException
+//package Deque.DoublyLinked;
+package com.company;
+
+/**
+   A class of runtime exceptions thrown by methods to
+   indicate that a queue is empty.
+   @author Frank M. Carrano
+   @author Timothy M. Henry
+*/
+public class EmptyQueueException extends RuntimeException
+{
+   public EmptyQueueException()
+   {
+      this(null);
+   } // end default constructor
+   
+   public EmptyQueueException(String message)
+   {
+      super(message);
+   } // end constructor
+} // end EmptyQueueException
