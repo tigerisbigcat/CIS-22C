5f60ee1fed83dbfff8eed0277db255eeb11837a6
Catherine Hao <31458785+tigerisbigcat@users.noreply.github.com>
2021-02-18 13:34:03 -0800

    Update TreeIteratorInterface.java

--- Exercise1-BinaryTree/TreePackage/TreeIteratorInterface.java
+++ Exercise1-BinaryTree/TreePackage/TreeIteratorInterface.java
@@ -1,1 +1,16 @@
-/**   An interface of iterators for the ADT tree.      @author Frank M. Carrano   @author Timothy M. Henry   @version 4.0*/package TreePackage;import java.util.Iterator;public interface TreeIteratorInterface<T>{   public Iterator<T> getPreorderIterator();   public Iterator<T> getPostorderIterator();   public Iterator<T> getInorderIterator();   public Iterator<T> getLevelOrderIterator();} // end TreeIteratorInterface
+/**
+   An interface of iterators for the ADT tree.
+   
+   @author Frank M. Carrano
+   @author Timothy M. Henry
+   @version 4.0
+*/
+package TreePackage;
+import java.util.Iterator;
+public interface TreeIteratorInterface<T>
+{
+   public Iterator<T> getPreorderIterator();
+   public Iterator<T> getPostorderIterator();
+   public Iterator<T> getInorderIterator();
+   public Iterator<T> getLevelOrderIterator();
+} // end TreeIteratorInterface
