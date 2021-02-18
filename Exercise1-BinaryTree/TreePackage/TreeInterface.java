a1ad09765c59501fdf7a39d2c8dfa948d1cefc9d
Hao <xiayushan2003@gmail.com>
2021-02-18 13:32:54 -0800

    upload

---
+++ Exercise1-BinaryTree/TreePackage/TreeInterface.java
@@ -0,0 +1,1 @@
+/**   An interface of basic methods for the ADT tree.      @author Frank M. Carrano   @author Timothy M. Henry   @version 4.0*/package TreePackage;public interface TreeInterface<T>{   public T getRootData();   public int getHeight();   public int getNumberOfNodes();   public boolean isEmpty();   public void clear();} // end TreeInterface
