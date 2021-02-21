654f0afe599e6fff8e82b0a74cee986ec367fd41
Catherine Hao <31458785+tigerisbigcat@users.noreply.github.com>
2021-02-20 19:14:47 -0800

    Update EmptyTreeException.java

--- Exercise2-BinarySearchTree/EmptyTreeException.java
+++ Exercise2-BinarySearchTree/EmptyTreeException.java
@@ -1,1 +1,21 @@
-/**   A class of runtime exceptions thrown by methods to   indicate that a tree is empty.   @author Frank M. Carrano   @author Timothy M. Henry   @version 4.2 */public class EmptyTreeException extends RuntimeException{	public EmptyTreeException()	{		this(null);	} // end default constructor	public EmptyTreeException(String message)	{		super(message);	} // end constructor} // end EmptyTreeException
+
+/**
+   A class of runtime exceptions thrown by methods to
+   indicate that a tree is empty.
+
+   @author Frank M. Carrano
+   @author Timothy M. Henry
+   @version 4.2
+ */
+public class EmptyTreeException extends RuntimeException
+{
+	public EmptyTreeException()
+	{
+		this(null);
+	} // end default constructor
+
+	public EmptyTreeException(String message)
+	{
+		super(message);
+	} // end constructor
+} // end EmptyTreeException
