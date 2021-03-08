94add2b82c60c91c40378f77d3bdf0e1be433d65
Catherine Hao <31458785+tigerisbigcat@users.noreply.github.com>
2021-03-07 19:42:52 -0800

    Update GraphInterface.java

--- Exercise1-Directed Weighted/GraphInterface.java
+++ Exercise1-Directed Weighted/GraphInterface.java
@@ -1,1 +1,12 @@
-/**   An interface of methods that create, manipulate, and process a graph.      @author Frank M. Carrano   @author Timothy M. Henry   @version 4.0*/public interface GraphInterface<T> extends BasicGraphInterface<T>,                                            GraphAlgorithmsInterface<T>{} // end GraphInterface
+
+/**
+   An interface of methods that create, manipulate, and process a graph.
+   
+   @author Frank M. Carrano
+   @author Timothy M. Henry
+   @version 4.0
+*/
+public interface GraphInterface<T> extends BasicGraphInterface<T>, 
+                                           GraphAlgorithmsInterface<T>
+{
+} // end GraphInterface
